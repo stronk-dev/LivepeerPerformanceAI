02b135e583ced0ec54c7e9e2b40c5c10c389244b
Marco van Dijk <marco@stronk.rocks>
2024-11-23 01:49:48 +0100

    better heatmap tooltip

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./heatmap.css";
 
 const Heatmap = ({ rawData }) => {
+  const [hoverInfo, setHoverInfo] = useState(null);
+  const tooltipRef = useRef(null);
   const data = rawData.data;
   const uniqueCombos = {};
 
@@ -30,14 +32,18 @@ const Heatmap = ({ rawData }) => {
         ethAddr: ethAddr,
         totalScore: 0,
         totalSuccessRate: 0,
+        successResults: [],
         totalRoundTrip: 0,
+        roundTripResults: [],
         count: 0,
       };
     }
 
     acc[ethAddr].combos[job].totalScore += entry.score;
     acc[ethAddr].combos[job].totalSuccessRate += entry.success_rate;
+    acc[ethAddr].combos[job].successResults.push({ region: entry.region, value: entry.success_rate });
     acc[ethAddr].combos[job].totalRoundTrip += entry.round_trip_score;
+    acc[ethAddr].combos[job].roundTripResults.push({ region: entry.region, value: entry.round_trip_score });
     acc[ethAddr].combos[job].count += 1;
 
     acc[ethAddr].totalNodeScore += entry.score;
@@ -60,12 +66,14 @@ const Heatmap = ({ rawData }) => {
       node,
       totalNodeScore,
       combos: Object.entries(combos).map(
-        ([combo, { totalScore, totalRoundTrip, totalSuccessRate, model, ethAddr, pipeline, count }]) => ({
+        ([combo, { totalScore, totalRoundTrip, totalSuccessRate, model, ethAddr, pipeline, count, successResults, roundTripResults }]) => ({
           combo,
           ethAddr: ethAddr,
           model: model,
           pipeline: pipeline,
           totalScore: totalScore,
+          successResults: successResults,
+          roundTripResults: roundTripResults,
           averageScore: totalScore / count,
           averageSuccessRate: totalSuccessRate / count,
           averageRoundTrip: totalRoundTrip / count,
@@ -78,15 +86,20 @@ const Heatmap = ({ rawData }) => {
     (a, b) => comboScores[b] - comboScores[a]
   );
 
-  const [hoverInfo, setHoverInfo] = useState(null);
-
   const handleMouseMove = (entry, e) => {
     const viewportWidth = window.innerWidth;
     const viewportHeight = window.innerHeight;
-    const tooltipWidth = 260; // Estimated tooltip width
-    const tooltipHeight = 100; // Estimated tooltip height
+    var tooltipWidth = 260; // Default as fallback
+    var tooltipHeight = 100; // Default as fallback
+
     let x = e.clientX + 15; // Default offset
     let y = e.clientY + 15;
+
+    if (tooltipRef.current) {
+      const rect = tooltipRef.current.getBoundingClientRect();
+      tooltipWidth = rect.width;
+      tooltipHeight = rect.height;
+    }
 
     // Adjust for viewport boundaries
     if (x + tooltipWidth > viewportWidth) x = viewportWidth - tooltipWidth - 15;
@@ -156,6 +169,7 @@ const Heatmap = ({ rawData }) => {
       </div>
       {hoverInfo && (
         <div
+          ref={tooltipRef}
           className="heatmap-tooltip"
           style={{
             top: `${hoverInfo.y}px`,
@@ -171,26 +185,42 @@ const Heatmap = ({ rawData }) => {
           <div className="heatmap-tooltip-subsubheader">
             <span>{hoverInfo.ethAddr}</span>
           </div>
-          <div className="heatmap-tooltip-divider"></div>
+          <div className="heatmap-tooltip-row">
+            <span className="heatmap-tooltip-row-label">Score:</span>
+            <span className="heatmap-tooltip-row-value">
+              {hoverInfo.averageScore.toFixed(2) * 100}%
+            </span>
+          </div>
+          <div className="heatmap-tooltip-divider" />
           <div className="heatmap-tooltip-body">
-            <div className="heatmap-tooltip-row">
-              <span className="heatmap-tooltip-row-label">Average Score:</span>
-              <span className="heatmap-tooltip-row-value">
-                {hoverInfo.averageScore.toFixed(2)}
-              </span>
-            </div>
             <div className="heatmap-tooltip-row">
               <span className="heatmap-tooltip-row-label">Success Rate:</span>
               <span className="heatmap-tooltip-row-value">
-                {hoverInfo.averageSuccessRate.toFixed(2)}
+                {hoverInfo.averageSuccessRate.toFixed(2) * 100}%
               </span>
             </div>
+            {hoverInfo.successResults.map((obj, idx) => (
+              <div className="heatmap-tooltip-row" key={"successrate" + obj.value + idx}>
+                <span className="heatmap-tooltip-row-label" style={{ fontSize: "10px", fontWeight: "normal" }}>{obj.region}</span>
+                <span className="heatmap-tooltip-row-value" style={{ fontSize: "10px", fontWeight: "normal" }}>
+                  {obj.value.toFixed(2) * 100}%
+                </span>
+              </div>
+            ))}
             <div className="heatmap-tooltip-row">
               <span className="heatmap-tooltip-row-label">Round Trip:</span>
               <span className="heatmap-tooltip-row-value">
-                {hoverInfo.averageRoundTrip.toFixed(2)}
+                {hoverInfo.averageRoundTrip.toFixed(2) * 100}%
               </span>
             </div>
+            {hoverInfo.roundTripResults.map((obj, idx) => (
+              <div className="heatmap-tooltip-row" key={"roundtrip" + obj.value + idx}>
+                <span className="heatmap-tooltip-row-label" style={{ fontSize: "10px", fontWeight: "normal" }}>{obj.region}</span>
+                <span className="heatmap-tooltip-row-value" style={{ fontSize: "10px", fontWeight: "normal" }}>
+                  {obj.value.toFixed(2) * 100}%
+                </span>
+              </div>
+            ))}
           </div>
         </div>
       )}
